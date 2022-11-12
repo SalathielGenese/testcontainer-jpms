9dbf215569422bccdda905b281fed4fa719fa50b
Salathiel Genese <salathiel@genese.name>
2022-11-12 08:59:36 +0100

    feat: add testcontainer and use HTTPD alpine to set up test case

--- src/test/java/name/genese/salathiel/testcontainerjpms/HttpdTest.java
+++ src/test/java/name/genese/salathiel/testcontainerjpms/HttpdTest.java
@@ -1,9 +1,29 @@
 package name.genese.salathiel.testcontainerjpms;
 
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+@Testcontainers
 public class HttpdTest {
+    @Container
+    @SuppressWarnings("resource")
+    public GenericContainer<?> httpd = new GenericContainer<>(
+            DockerImageName.parse("httpd:alpine")
+    ).withExposedPorts(80);
+
     @Test
-    void noop() {
+    void noop() throws IOException {
+        final var bytes = new URL("http://0.0.0.0:%d".formatted(httpd.getMappedPort(80)))
+                .openConnection()
+                .getInputStream()
+                .readAllBytes();
+        System.out.println(new String(bytes));
     }
 }
