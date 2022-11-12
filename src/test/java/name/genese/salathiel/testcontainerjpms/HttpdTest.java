3f5015fc739ab17b7c9bd7491e66ec03ec886ec0
Salathiel Genese <salathiel@genese.name>
2022-11-12 09:06:50 +0100

    feat: add assertion in test case

--- src/test/java/name/genese/salathiel/testcontainerjpms/HttpdTest.java
+++ src/test/java/name/genese/salathiel/testcontainerjpms/HttpdTest.java
@@ -7,8 +7,10 @@ import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
 import java.net.URL;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @Testcontainers
 public class HttpdTest {
@@ -24,6 +26,7 @@ public class HttpdTest {
                 .openConnection()
                 .getInputStream()
                 .readAllBytes();
-        System.out.println(new String(bytes));
+
+        assertEquals(new String(bytes, UTF_8), "<html><body><h1>It works!</h1></body></html>\n");
     }
 }
